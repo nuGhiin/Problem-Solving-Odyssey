3a522e5417cf5a610f3851a51e9631fb9964e397
nuGhinTanjid <tanjidit2@gmail.com>
2023-10-09 02:51:19 +0600

    5

---
+++ maxMin.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+     if(a<=b && a<=c){
+        printf("%d ", a);
+    }
+    else if(b<=a && b<=c){
+        printf("%d ", b);
+    }
+    else{
+        printf("%d ", c);
+    }//min
+    
+    
+    
+    if(a>=b && a>=c){
+        printf("%d", a);
+    }
+    else if(b>=a && b>=c){
+        printf("%d", b);
+    }
+    else{
+        printf("%d", c);
+    } //max
+
+    
+
+return 0;
+}
