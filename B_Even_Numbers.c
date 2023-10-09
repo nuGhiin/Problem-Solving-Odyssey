6d06049c092b1a7e4d317a42b3a51b7747c71028
nuGhinTanjid <tanjidit2@gmail.com>
2023-10-09 22:47:15 +0600

    even

---
+++ B_Even_Numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+int x;
+scanf("%d", &x);
+
+if(x==1){
+    printf("-1");
+}
+else
+{
+    for(int i=1; i<=x; i++)
+    {
+    if(i%2==0)
+    {
+        printf("%d\n", i);
+    }
+    }
+}
+
+
+return 0;
+}
