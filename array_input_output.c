b71ed7329c5d74bba6cac9cbed87f9c0e1055e5e
nuGhinTanjid <tanjidit2@gmail.com>
2023-10-12 14:37:08 +0600

    sum

--- array_input_output.c
+++ array_input_output.c
@@ -1,24 +1,40 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main(){
+
+//     int arr[5];
+//     // arr[0]=100;
+//     // arr[1]=255;
+//     // arr[2]=555;
+//     // arr[3]=455;
+
+//     for(int i=0; i<5; i++){
+//         // printf("arr[%d]\n", i); for printing array
+//         scanf("%d", &arr[i]);
+//     }
+
+//      for(int i=0; i<5; i++){
+//          printf("%d\n", arr[i]); 
+        
+//     }
+// return 0;
+// }
+
+#include <stdio.h>// sum of array
 int main(){
 
-    int arr[5];
-    // arr[0]=100;
-    // arr[1]=255;
-    // arr[2]=555;
-    // arr[3]=455;
+    int n;
+    int arr[n];
+    scanf("%d", &n);
 
-    for(int i=0; i<5; i++){
-        // printf("arr[%d]\n", i); for printing array
+    for(int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-
-     for(int i=0; i<5; i++){
-         printf("%d\n", arr[i]); 
-        
+    
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum=sum+arr[i];
     }
-
-
-
+    printf("%d", sum);
 
 
 return 0;
