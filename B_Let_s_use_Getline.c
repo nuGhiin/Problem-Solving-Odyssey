90d836fd506067fe5b63cfdbb5492df9e279c39d
nuGhinTanjid <tanjidit2@gmail.com>
2023-10-18 15:36:48 +0600

    fgets

---
+++ B_Let_s_use_Getline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+     char s[1000001];
+    fgets(s, 100001, stdin);
+
+    int i=0;
+    while(s[i] != '\\'){
+        printf("%c", s[i]);
+        i++;
+    }
+
+    
+
+return 0;
+}
