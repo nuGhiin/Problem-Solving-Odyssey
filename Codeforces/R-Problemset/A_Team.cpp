fb79342bfea2dd5a2694378688a3ae7df070f811
nuGhinTanjid <tanjidit2@gmail.com>
2023-11-20 22:39:22 +0600

    solved

--- Codeforces/R-Problemset/A_Team.cpp
+++ Codeforces/R-Problemset/A_Team.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
 
-    
+int main() {
+    int n;
+    cin >> n;
+    int a[3];
+    int flag = 0;
 
-return 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> a[j];
+            //cout << a[j] << " ";
+        }
+
+        if ((a[0] == 1 && a[1] == 1) || (a[0] == 1 && a[2] == 1) || (a[1] == 1 && a[2] == 1)) {
+            flag++;
+        }
+
+        cout << endl;
+    }
+    cout << flag << endl;
+
+    return 0;
 }
