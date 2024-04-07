267ff26b76129107347ad8be523a9b6fa9e06aad
nuGhinTanjid <tanjidit2@gmail.com>
2024-04-07 23:54:02 +0600

    .

---
+++ XPSC/Week2/C_Move_Brackets.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        string s; cin>>s;
+        int o=0, c=0, ans=0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='('){
+                o++;
+            }
+            else{
+                if(o>0){
+                    o--;
+                }
+                else if(o==0){
+                    ans++;
+                }
+            }
+
+        }
+        cout<<ans<<endl;
+
+    }
+
+return 0;
+}
