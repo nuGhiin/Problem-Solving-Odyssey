08101e0cc846e9ff0d73e86597e072ac1945caf6
nuGhinTanjid <tanjidit2@gmail.com>
2024-05-22 22:25:20 +0600

    boom

---
+++ Codechef/div4_22524/RCB_vs_CSK.cpp
@@ -0,0 +1,12 @@
+/*p: https://www.codechef.com/START135D/problems/RCBCSK*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y; cin>>x>>y;
+	int res=x-y;
+	if(res>=18) cout<<"RCB";
+	else cout<<"CSK";
+
+}
