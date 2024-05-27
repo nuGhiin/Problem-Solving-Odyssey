5f63fe7cfab14ba3bf7cc62658a45a265e865546
nuGhinTanjid <tanjidit2@gmail.com>
2024-05-27 22:50:41 +0600

    ggg

---
+++ XPSC/Week7/C_Two_Arrays.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v1(n), v2(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v1[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v2[i];
+        }
+        sort(v1.begin(), v1.end());
+        sort(v2.begin(), v2.end());
+
+        bool flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (v1[i] != v2[i])
+            {
+                if (v1[i] + 1 != v2[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
